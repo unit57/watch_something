bd064fcf5ce32acde55c46336d16467ae3c803d6
Eric Sanchez <eric.q.sanchez@gmail.com>
2017-06-13 11:09:04 -0400

    scaffold react app

--- watchsomething-app/watchsomething-app/src/App.js
+++ watchsomething-app/watchsomething-app/src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-         
-          <h2>Welcome to Watch Something</h2>
-        </div>
+          <h2> Watch Something</h2>
+          
+          <div id="navComponent">
+          	<nav>
+          	choose
+          	<select>
+          		<option> comedy </option>
+          		<option> horror </option>
+          		<option> drama  </option>
+          	</select>
+          	<select>
+          		<option> 2017 </option>
+          		<option> 2016 </option>
+          	</select>
+          	<button> submit </button>
+          	</nav>
+          </div>
+
+          <div id="movieComponent"> 
+          	<div id="movieResults">
+	          	<div id="movieContainer">
+	          		<div className="imageContainer">
+	          			<img src="#"></img>
+	          	</div>
+	          		<button> select </button>
+	          	</div>
+	         </div>
+          </div>
+   
+          <div id="selectedMoviePage">
+          		<div id="movieAndDescription">
+          			<img src="#"></img>
+          			<p>title</p>
+          			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eveniet pariatur illo alias non, voluptatem natus quo, obcaecati ipsum numquam doloremque doloribus assumenda, dicta sapiente atque eligendi odio, itaque officiis!</p>
+          		</div>
+          	<div id="amazonResults">
+          	 AMAZON RESULTS HERE
+          	</div>
+
+
+          </div>
+
        
       </div>
     );
